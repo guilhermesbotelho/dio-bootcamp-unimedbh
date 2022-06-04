fe3d53bbdbe36478f165d0702c08e8a47ef0c6ce
Guilherme Siqueira Botelho <guilhermesbotelho@outlook.com>
2022-06-03 23:47:03 -0300

    correcoes de identacao

--- modulo-ii/index.php
+++ modulo-ii/index.php
@@ -1,10 +1,8 @@
 <html>
     <body>
-
         <form action="/piramides.php" method="post">
             Tamanho da Pirâmide: <input type="number" name="tamanho_piramide"><br>
             <input type="submit">
         </form>
-
     </body>
 </html>
