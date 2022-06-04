533cae28b281c549a646f60bef404e4bd77c0894
Guilherme Siqueira Botelho <guilhermesbotelho@outlook.com>
2022-06-03 21:03:53 -0300

    Identar

--- modulo-ii/index.php
+++ modulo-ii/index.php
@@ -1,10 +1,10 @@
 <html>
-<body>
+    <body>
 
-<form action="/piramides.php" method="post">
-Tamanho da Pirâmide: <input type="number" name="tamanho_piramide"><br>
-<input type="submit">
-</form>
+        <form action="/piramides.php" method="post">
+            Tamanho da Pirâmide: <input type="number" name="tamanho_piramide"><br>
+            <input type="submit">
+        </form>
 
-</body>
+    </body>
 </html>
